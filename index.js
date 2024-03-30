b98dc3c8ae1b2f23316957f26dfb0775bccba129
Gloria Wekura <wekuraglorian07@gmail.com>
2024-03-30 14:19:11 +0300

    submit assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+
+let multiply = num1*num2
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 10)
+Number.isInteger(random)
+console.log(random)
+
+const num3 = 4
+const num4 = 7
+
+let mod = num3 % num4
+console.log(mod)
+
+const max = Math.max(5, 17, 2, 20, 13)
+console.log(max)
